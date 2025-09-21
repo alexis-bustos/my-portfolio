79ea221826e2af76e83d3d770cc60cbcf4fecb8c
Alexis Bustos <alexisbustos10@yahoo.com>
2025-09-21 16:01:25 -0700

    updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
                 Get in Touch
               </a>
               <a
-                href="/assets/Resume_Alexis_Bustos_09_21.pdf"
+                href="/assets/Resume_Alexis_Bustos_09_21_25.pdf"
                 download="Resume_Alexis_Bustos.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                 Download CV/Résumé
