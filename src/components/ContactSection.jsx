d5b02230b5189ec591a82b2db576d1a34ec37877
Alexis Bustos <alexisbustos10@yahoo.com>
2025-09-21 15:50:13 -0700

    modified column for ContactSection component

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -35,7 +35,7 @@ export const ContactSection = () => {
           I'm always open to discussing new opportunities.
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-12">
           <div className="space-y-8">
             <h3 className="text-2xl font-semibold mb-6">
               {" "}
